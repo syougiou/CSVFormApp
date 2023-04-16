5a530ba1795c700fa5eda3daf9b66bc10ffa4810
Araya515 <tomoya5.15@me.com>
2023-04-16 22:47:50 +0900

    プロジェクト ファイルを追加します。

---
+++ WindowsFormsApp1/CSV.Designer.cs
@@ -0,0 +1,129 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class CSV
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.FileSelect = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label2 = new System.Windows.Forms.Label();
+            this.csvbutton = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.FileSelect});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(370, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // FileSelect
+            // 
+            this.FileSelect.Name = "FileSelect";
+            this.FileSelect.Size = new System.Drawing.Size(77, 20);
+            this.FileSelect.Text = "ファイル選択";
+            this.FileSelect.Click += new System.EventHandler(this.FileSelect_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(87, 58);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(162, 19);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(52, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 12);
+            this.label1.TabIndex = 2;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "ファイル名";
+            // 
+            // csvbutton
+            // 
+            this.csvbutton.Location = new System.Drawing.Point(265, 56);
+            this.csvbutton.Name = "csvbutton";
+            this.csvbutton.Size = new System.Drawing.Size(75, 23);
+            this.csvbutton.TabIndex = 4;
+            this.csvbutton.Text = "csv解析";
+            this.csvbutton.UseVisualStyleBackColor = true;
+            this.csvbutton.Click += new System.EventHandler(this.csvbutton);
+            // 
+            // CSV
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(370, 121);
+            this.Controls.Add(this.csvbutton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "CSV";
+            this.Text = "CSVアプリ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem FileSelect;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button csvbutton;
+    }
+}
+
---
+++ WindowsFormsApp1/CSV.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class CSV : Form
+    {
+        string fullpath = @"C:\";
+
+        public CSV()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// CSVファイルを選択します
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void FileSelect_Click(object sender, EventArgs e)
+        {
+            //OpenFileDialogクラスのインスタンスを作成
+            OpenFileDialog ofd = openFileDialog1;
+
+            //はじめのファイル名を指定する
+            //はじめに「ファイル名」で表示される文字列を指定する
+            ofd.FileName = "default.csv";
+            //はじめに表示されるフォルダを指定する
+            //指定しない（空の文字列）の時は、現在のディレクトリが表示される
+            ofd.InitialDirectory = @"C:\";
+            //[ファイルの種類]に表示される選択肢を指定する
+            //指定しないとすべてのファイルが表示される
+            ofd.Filter = "CSV UTF-8 (コンマ区切り)(*.csv)|*.csv";
+            //タイトルを設定する
+            ofd.Title = "ファイルを選択してください";
+            //ダイアログボックスを閉じる前に現在のディレクトリを復元するようにする
+            ofd.RestoreDirectory = true;
+            //存在しないファイルの名前が指定されたとき警告を表示する
+            //デフォルトでTrueなので指定する必要はない
+            ofd.CheckFileExists = true;
+            //存在しないパスが指定されたとき警告を表示する
+            //デフォルトでTrueなので指定する必要はない
+            ofd.CheckPathExists = true;
+
+            //ダイアログを表示する
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                fullpath = ofd.FileName;
+
+                string path = Path.GetFileName(ofd.FileName);
+
+                //OKボタンがクリックされたとき、選択されたファイル名を表示する
+                textBox1.Text = path;
+            }
+        }
+
+        private void csvbutton(object sender, EventArgs e)
+        {
+            StreamReader sr = new StreamReader(fullpath, System.Text.Encoding.UTF8);
+
+            CsvToArrayList2(sr.ReadToEnd());
+        }
+
+        /// <summary>
+        /// CSVをArrayListに変換
+        /// https://dobon.net/vb/dotnet/file/readcsvfile.html#section6
+        /// </summary>
+        /// <param name="csvText">CSVの内容が入ったString</param>
+        /// <returns>変換結果のArrayList</returns>
+        public static System.Collections.ArrayList CsvToArrayList2(string csvText)
+        {
+            //前後の改行を削除しておく
+            csvText = csvText.Trim(new char[] { '\r', '\n' });
+
+            System.Collections.ArrayList csvRecords =
+                new System.Collections.ArrayList();
+            System.Collections.ArrayList csvFields =
+                new System.Collections.ArrayList();
+
+            int csvTextLength = csvText.Length;
+            int startPos = 0, endPos = 0;
+            string field = "";
+
+            while (true)
+            {
+                //空白を飛ばす
+                while (startPos < csvTextLength &&
+                    (csvText[startPos] == ' ' || csvText[startPos] == '\t'))
+                {
+                    startPos++;
+                }
+
+                //データの最後の位置を取得
+                if (startPos < csvTextLength && csvText[startPos] == '"')
+                {
+                    //"で囲まれているとき
+                    //最後の"を探す
+                    endPos = startPos;
+                    while (true)
+                    {
+                        endPos = csvText.IndexOf('"', endPos + 1);
+                        if (endPos < 0)
+                        {
+                            throw new ApplicationException("\"が不正");
+                        }
+                        //"が2つ続かない時は終了
+                        if (endPos + 1 == csvTextLength || csvText[endPos + 1] != '"')
+                        {
+                            break;
+                        }
+                        //"が2つ続く
+                        endPos++;
+                    }
+
+                    //一つのフィールドを取り出す
+                    field = csvText.Substring(startPos, endPos - startPos + 1);
+                    //""を"にする
+                    field = field.Substring(1, field.Length - 2).Replace("\"\"", "\"");
+
+                    endPos++;
+                    //空白を飛ばす
+                    while (endPos < csvTextLength &&
+                        csvText[endPos] != ',' && csvText[endPos] != '\n')
+                    {
+                        endPos++;
+                    }
+                }
+                else
+                {
+                    //"で囲まれていない
+                    //カンマか改行の位置
+                    endPos = startPos;
+                    while (endPos < csvTextLength &&
+                        csvText[endPos] != ',' && csvText[endPos] != '\n')
+                    {
+                        endPos++;
+                    }
+
+                    //一つのフィールドを取り出す
+                    field = csvText.Substring(startPos, endPos - startPos);
+                    //後の空白を削除
+                    field = field.TrimEnd();
+                }
+
+                //フィールドの追加
+                csvFields.Add(field);
+
+                //行の終了か調べる
+                if (endPos >= csvTextLength || csvText[endPos] == '\n')
+                {
+                    //行の終了
+                    //レコードの追加
+                    csvFields.TrimToSize();
+                    csvRecords.Add(csvFields);
+                    csvFields = new System.Collections.ArrayList(
+                        csvFields.Count);
+
+                    if (endPos >= csvTextLength)
+                    {
+                        //終了
+                        break;
+                    }
+                }
+
+                //次のデータの開始位置
+                startPos = endPos + 1;
+            }
+
+            csvRecords.TrimToSize();
+            return csvRecords;
+        }
+    }
+}
